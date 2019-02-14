c7567654d8b330d072714c578eea494cd08fbf01
cozafiu <costinel.zafiu@1and1.ro>
2019-02-14 18:15:47 +0200

    small change

--- graphs.py
+++ graphs.py
@@ -28,6 +28,15 @@ def getDataFrame(statsUrl, jsonBody):
 
 	return dataFrame;
 
+def getPopulationYearByYearDataFrame():
+	populationUrl = mainUrl + "BE0101A/BefolkningNy"
+	requestBodyYearByYear = {"query":[{"code":"Region","selection":{"filter":"vs:RegionKommun07","values":["0885"]}},{"code":"Alder","selection":{"filter":"vs:Ålder1årA","values":["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60","61","62","63","64","65","66","67","68","69","70","71","72","73","74","75","76","77","78","79","80","81","82","83","84","85","86","87","88","89","90","91","92","93","94","95","96","97","98","99","100+"]}},{"code":"Kon","selection":{"filter":"item","values":["1","2"]}},{"code":"ContentsCode","selection":{"filter":"item","values":["BE0101N1"]}}],"response":{"format":"json"}}
+	matrix = [];
+	
+	response = request.post(url = populationUrl, json = requestBodyYearByYear, headers = headers);
+	print response.text
+	json_data = simplejson.loads(response.text)["data"]
+
 def getPopulationByGenderDataframe():
 	populationUrl = mainUrl + "BE0101A/BefolkningNy"
 	requestBodyForPopulation = {"query":[{"code":"Region","selection":{"filter":"vs:RegionKommun07","values":["0885"]}},{"code":"Kon","selection":{"filter":"item","values":["1","2"]}},{"code":"ContentsCode","selection":{"filter":"item","values":["BE0101N1"]}}],"response":{"format":"json"}};
@@ -135,10 +144,11 @@ def plotMoveoutsByGenderGraph():
 
 
 if __name__ == "__main__":
-	plotPopulationByGenderGraph()
-	plotBirthsByGenderGraph()
-	plotDeathsByGenderGraph()
-	plotImmigrationByGenderGraph()
-	plotEmigrationByGenderGraph()
-	plotMoveinsByGenderGraph()
-	plotMoveoutsByGenderGraph()
+	# plotPopulationByGenderGraph()
+	# plotBirthsByGenderGraph()
+	# plotDeathsByGenderGraph()
+	# plotImmigrationByGenderGraph()
+	# plotEmigrationByGenderGraph()
+	# plotMoveinsByGenderGraph()
+	# plotMoveoutsByGenderGraph()
+	getPopulationYearByYearDataFrame()
