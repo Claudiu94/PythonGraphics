196cf26c8b871a279c47f0e8a78d024d44158a90
cozafiu <costinel.zafiu@1and1.ro>
2019-03-01 17:22:04 +0200

    started work for second project

---
+++ graphsB.py
@@ -0,0 +1,6 @@
+from plotly.offline import download_plotlyjs, plot
+import dataFrameCrawler as dataFrameService
+import plotly.graph_objs as go
+
+if __name__ == "__main__":
+	dataFrameService.getNumberOfAppartmentsData()
